0deac237aab0c1a3026c33360cd328032af48d1b
torsimulator <torsimulator@gmail.com>
2014-03-22 17:36:35 -0400

    testing config: 1 client with 1 bulk download

---
+++ config/testing-N23/1-client-1-bulk-download/summarize.r
@@ -0,0 +1,10 @@
+first_bytes=scan("first_bytes.log")
+first_mean = mean(first_bytes)
+first_var = var(first_bytes)
+
+download_time=scan("download_time.log")
+download_mean = mean(download_time)
+download_var = var(download_time)
+
+print(cat("\n\nTime to first byte:\nmean ",first_mean, "\nvariance ",first_var,"\n\nDownload time:\nmean ",download_mean,"\nvariance ",download_var,"\n"))
+
