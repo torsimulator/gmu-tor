094efd4ab5340f16604ce68b6c9910f179d696c4
vsridhar1729 <svenkate@masonlive.gmu.edu>
2014-03-24 02:58:43 -0400

    Debugging by NOT sending the flowcontrol cell

--- tor-0.2.4.21/src/or/relay.c
+++ tor-0.2.4.21/src/or/relay.c
@@ -2587,6 +2587,7 @@ channel_consider_sending_flowcontrol_cell(int cell_direction, int nBuffer, circu
     */
     tor_assert(chan);
     if(cell_direction == CELL_DIRECTION_IN){
+        log_debug(LD_CHANNEL,"CELL_DIRECTION_IN");
 
         circ_id = circ->n_circ_id;
         previous_chan = circ->n_chan;
@@ -2595,13 +2596,15 @@ channel_consider_sending_flowcontrol_cell(int cell_direction, int nBuffer, circu
         credit_balance = (--or_circ->credit_balance_p);
         or_circ->cells_fwded_p++;
         if(or_circ->is_first_hop){
+            log_debug(LD_CHANNEL,"ENTRY ROUTER");
             if(!credit_balance)
                 or_circ->credit_balance_p = N2+N3;
             if(or_circ->cells_fwded_p % N2 ==0)
                 if(nBuffer < N2+N3)
-                    channel_send_flowcontrol(circ_id,previous_chan,or_circ->cells_fwded_p);
+                    //channel_send_flowcontrol(circ_id,previous_chan,or_circ->cells_fwded_p);
         }
         else if (!circ->n_chan){ //Exit
+            log_debug(LD_CHANNEL,"EXIT ROUTER");
             //Make streams inactive if credit_balance is less than 0
 
             /*edge_connection_t *conn=NULL;
@@ -2614,34 +2617,39 @@ channel_consider_sending_flowcontrol_cell(int cell_direction, int nBuffer, circu
         }
         else {//Middle
             //if(credit_balance <=0) circuitmux_set_num_cells(chan->cmux,circ,0);
+            log_debug(LD_CHANNEL,"MIDDLE ROUTER");
             if(or_circ->cells_fwded_p % N2 ==0)
-                if(nBuffer<N2+N3) channel_send_flowcontrol(circ_id,previous_chan,or_circ->cells_fwded_p);
+                //if(nBuffer<N2+N3) channel_send_flowcontrol(circ_id,previous_chan,or_circ->cells_fwded_p);
 
             }
     }
     else{    //Heading towards exit
 
+        log_debug(LD_CHANNEL,"CELL_DIRECTION_OUT");
         or_circ = TO_OR_CIRCUIT(circ);
         circ_id = or_circ->p_circ_id;
         previous_chan = or_circ->p_chan;
         credit_balance = (++circ->credit_balance_n);
         circ->cells_fwded_n++;
         if(or_circ->is_first_hop){//Entry
+            log_debug(LD_CHANNEL,"ENTRY ROUTER");
             // Need to make the corresponding p_streams also inactive?
             //edge_connection_t *conn=NULL;
             //if(credit_balance <=0) circuitmux_set_num_cells(chan->cmux,circ,0);
 
         }
         else if(!circ->n_chan){ //Exit
+            log_debug(LD_CHANNEL,"EXIT ROUTER");
             if(!credit_balance)
                 circ->credit_balance_n = N2+N3;
             if(circ->cells_fwded_n % N2 ==0)
-                if(nBuffer < N2+N3)channel_send_flowcontrol(circ_id,previous_chan,circ->cells_fwded_n);
+                //if(nBuffer < N2+N3)channel_send_flowcontrol(circ_id,previous_chan,circ->cells_fwded_n);
         }
         else{//Middle
+            log_debug(LD_CHANNEL,"MIDDLE ROUTER");
             //if(credit_balance <=0) circuitmux_set_num_cells(chan->cmux,circ,0);
             if(circ->cells_fwded_n % N2 ==0)
-                if(nBuffer<N2+N3) channel_send_flowcontrol(circ_id,previous_chan,circ->cells_fwded_n);
+                //if(nBuffer<N2+N3) channel_send_flowcontrol(circ_id,previous_chan,circ->cells_fwded_n);
 
         }
     }
