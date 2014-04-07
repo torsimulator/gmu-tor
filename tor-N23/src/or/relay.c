c3968d020fae122c56555e638b496c8c603f7082
vsridhar1729 <svenkate@masonlive.gmu.edu>
2014-04-07 01:53:54 -0400

    Logging info with time

--- tor-N23/src/or/relay.c
+++ tor-N23/src/or/relay.c
@@ -2746,7 +2746,7 @@ connection_or_consider_sending_flowcontrol_cell(int cell_direction_p, int nBuffe
     int credit_balance = 0;
     or_connection_t *previous_or = NULL;
     circid_t circ_id;
-    static time_t log_time_start(0);
+    static time_t log_time_start=0;
 
     time_t now = time(NULL);
     double seconds = difftime(now,log_time_start);
