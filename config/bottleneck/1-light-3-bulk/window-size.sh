a39e54d6bbb25254e9890e88981e88394110a6b6
torsimulator <torsimulator@gmail.com>
2014-05-01 02:16:28 -0400

    Window log of 100 downloads

--- config/bottleneck/1-light-3-bulk/window-size.sh
+++ config/bottleneck/1-light-3-bulk/window-size.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-grep "CIRCUIT," data/scallion.log > data/window.log
-zip window-log.zip data/window.log
+for i in $(seq 1 1 $1)
+do
+	scallion -i try1.xml
+	grep "CIRCUIT, STREAM" data/scallion.log | cut -d ':' -f5 > data/window.log
+	file="window-$i"
+	zip "$file.zip" data/window.log
+	rm  -r data/
+done
