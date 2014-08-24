834a991a72d2f9ff7b3ac602836fa09e6b698919
vsridhar1729 <svenkate@masonlive.gmu.edu>
2014-08-23 22:35:39 -0400

    Moving and renaming configs in testing-N23

--- config/testing-N23/1-client-1-light-download/plotData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-grep  "download-complete" data/scallion.log > temp.log
-sed -e 's/^.*\(first bytes in [^ ]*\).*$/\1/' temp.log | tr ' ' '\n' | sed -rne "/([0-9])/P" > first_bytes.log
-sed -e 's/^.*\(bytes in [^ ]*\).*$/\1/' temp.log | tr ' ' '\n' | sed -rne "/([0-9])/P" > download_time.log
-#Rscript summarize.r
